2cd35214562d35025514d1ff0de8b33363bb1476
dahyunkim <dahyun_ee@naver.com>
2023-04-23 18:48:42 +0900

    [Add] 프로젝트 초기셋팅

---
+++ src/main/java/com/sopt/wokat/WokatApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.wokat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WokatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WokatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopt/wokat/WokatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.wokat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WokatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
