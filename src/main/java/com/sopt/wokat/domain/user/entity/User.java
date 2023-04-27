d7f7429a33b5ae80626e05cf539960f9d2542fed
dahyunkim <dahyun_ee@naver.com>
2023-04-27 15:24:28 +0900

    [Remove] queryDSL, jpa 관련 삭제

--- src/main/java/com/sopt/wokat/domain/user/entity/User.java
+++ src/main/java/com/sopt/wokat/domain/user/entity/User.java
@@ -1,18 +1,22 @@
 package com.sopt.wokat.domain.user.entity;
 
-import java.util.*;
+
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import lombok.*;
 import com.sopt.wokat.global.entity.BaseEntity;
 
-@EqualsAndHashCode(callSuper=false)
-@Entity @Data
-// @AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name= "users")
+
+@Getter
+@AllArgsConstructor()
+@Document("user")
 @Schema(description = "유저 테이블")
 public class User extends BaseEntity {
+    
+    private String userId;
+    private String userPw;
 
 }
+
+
